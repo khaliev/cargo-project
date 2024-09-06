a4312a5fc3d589bd430106c5b79b52ceaa52c27f
khaliev <dev.adlan@icloud.com>
2024-09-06 06:11:58 +0200

    personifying project4

--- src/components/complexFooter.tsx
+++ src/components/complexFooter.tsx
@@ -1,5 +1,4 @@
-import data from '../../public/data.json';
-import StoreDoubleColumn from '../components/store/storeDoubleColumn';
+import data from "../../public/data.json";
 
 export default function ComplexFooter() {
   return (
@@ -7,20 +6,23 @@ export default function ComplexFooter() {
       <footer>
         <div className="row">
           <div className="col-12 col-md-4">
-            <h5 className="mt-5">Astro Ecommerce</h5>
-            <p className="text-body text-sm pe-5">The time is now for it to be okay to be great. People in this world shun people for being great. For being a bright color. For standing out.</p>
+            <h5 className="mt-5">Cargo 917</h5>
+            <p className="text-body text-sm pe-5"></p>
           </div>
-          <div className="col-12 col-md-8">
-            <StoreDoubleColumn title={data.products[1].title}/>
-          </div>
+
           <div className="col-12 mt-5 mb-lg-0 mb-4">
             <div className="copyright text-center text-sm text-body">
-              Copyright © 
-              <script>
-                document.write(new Date().getFullYear())
-              </script>
-              &nbsp;Astro Ecommerce by &nbsp;
-              <a href="https://www.creative-tim.com" className="text-dark" target="_blank">Creative Tim</a>.
+              Copyright ©
+              <script>document.write(new Date().getFullYear())</script>
+              &nbsp;2024 &nbsp;
+              <a
+                href="https://www.creative-tim.com"
+                className="text-dark"
+                target="_blank"
+              >
+                Cargo 917
+              </a>
+              .
             </div>
           </div>
         </div>
@@ -28,5 +30,3 @@ export default function ComplexFooter() {
     </>
   );
 }
-
-
